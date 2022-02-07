6ce32378a1904764a5533b6b16a4dbf481d14c2f
matus.vid <matusko.vid@gmail.com>
2022-02-07 16:24:20 +0100

    Fix module storage

--- ex3/modules/storage/main.tf
+++ ex3/modules/storage/main.tf
@@ -1,15 +1,13 @@
 resource "aws_instance" "mongo" {
-  ami                    = "ami-02868af3c3df4b3aa"
+  ami                    = var.ami
   instance_type          = var.instance
   key_name               = var.keyname
 
   network_interface {
-    network_interface {
     device_index         = 0
-    network_interface_id = aws_network_interface.interface2.id
+    network_interface_id = 
   }
-  }
-
+  
   user_data = filebase64("${path.module}/install.sh")
 
   tags = {
